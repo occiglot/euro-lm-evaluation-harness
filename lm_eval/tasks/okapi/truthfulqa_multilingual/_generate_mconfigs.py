f0cad95109cf9f3810344d878a8eded8bcd52853
Fabio Barth <fabio.barth@dfki.de>
2024-01-29 17:02:52 +0100

    remove gen as subtask

--- lm_eval/tasks/okapi/truthfulqa_multilingual/_generate_mconfigs.py
+++ lm_eval/tasks/okapi/truthfulqa_multilingual/_generate_mconfigs.py
@@ -6,7 +6,7 @@ import os
 import yaml
 from tqdm import tqdm
 
-#from lm_eval.logger import eval_logger
+from lm_eval.logger import eval_logger
 
 LANGS = "ar,bn,ca,da,de,es,eu,fr,gu,hi,hr,hu,hy,id,it,kn,ml,mr,ne,nl,pt,ro,ru,sk,sr,sv,ta,te,uk,vi,zh".split(
     ","
@@ -16,9 +16,9 @@ LANGS = "ar,bn,ca,da,de,es,eu,fr,gu,hi,hr,hu,hy,id,it,kn,ml,mr,ne,nl,pt,ro,ru,sk
 SAFE_PATH = str(os.path.dirname(__file__)) + "/default/"
 
 if __name__ == "__main__":
-    variations = ['gen', 'mc1', 'mc2']
+    variations = ['mc1', 'mc2']
     for variation in variations:
-        for lang in tqdm(LANGS):
+        for lang in tqdm(LANGS, desc=f"Generate truthfulqa_{variation}"):
             # generate truthfulqa_gen 
             yaml_dict = {
                 "dataset_name": f"truthfulqa_{lang}",
@@ -29,7 +29,7 @@ if __name__ == "__main__":
                 "include": f"_default_multi_template_{variation}_yaml",
             }
             file_save_path = SAFE_PATH + f"truthfulqa_{variation}_m_{lang}.yaml"
-            #eval_logger.info(f"Saving yaml for multilanguage ({lang}) and {variation}-Task to {file_save_path}")
+            eval_logger.info(f"Saving yaml for multilanguage ({lang}) and {variation}-Task to {file_save_path}")
             with open(file_save_path, "w") as yaml_file:
                 yaml.dump(
                     yaml_dict,
